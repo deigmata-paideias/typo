698a6703097fd50cc6960e381d5e45c1fa37f7a7
yuluo-yx <yuluo08290126@gmail.com>
2026-01-19 21:48:48 +0800

    feat: add
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- internal/cmd/version/version.go
+++ internal/cmd/version/version.go
@@ -29,7 +29,7 @@ import (
 )
 
 type Info struct {
-	HCGVersion    string `json:"envoyGatewayVersion"`
+	TypoVersion   string `json:"envoyGatewayVersion"`
 	GitCommitID   string `json:"gitCommitID"`
 	GolangVersion string `json:"golangVersion"`
 }
@@ -37,14 +37,14 @@ type Info struct {
 func Get() Info {
 
 	return Info{
-		HCGVersion:    hcgVersion,
+		TypoVersion:   typoVersion,
 		GitCommitID:   gitCommitID,
 		GolangVersion: runtime.Version(),
 	}
 }
 
 var (
-	hcgVersion  string
+	typoVersion string
 	gitCommitID string
 )
 
@@ -63,7 +63,7 @@ func Print(w io.Writer, format string) error {
 			_, _ = fmt.Fprintln(w, string(marshalled))
 		}
 	default:
-		_, _ = fmt.Fprintf(w, "TYPO_GO_VERSION: %s\n", v.HCGVersion)
+		_, _ = fmt.Fprintf(w, "TYPO_GO_VERSION: %s\n", v.TypoVersion)
 		_, _ = fmt.Fprintf(w, "GIT_COMMIT_ID: %s\n", v.GitCommitID)
 		_, _ = fmt.Fprintf(w, "GOLANG_VERSION: %s\n", v.GolangVersion)
 	}
--- tools/make/golang.mk
+++ tools/make/golang.mk
@@ -17,11 +17,11 @@ vet: ## Golang vet
 
 .PHONY: dev-run
 dev-run: ## Golang dev, run main by run.
-	go run cmd/main.go server --config etc/hertzbeat-collector.yaml
+	go run cmd/main.go
 
 .PHONY: prod-run
 prod-run: ## Golang prod, run bin by run.
-	bin/collector server --config etc/hertzbeat-collector.yaml
+	bin/darwin/arm64/collector
 
 # 默认使用当前系统平台，可以通过参数覆盖：make build GOOS=linux GOARCH=amd64
 GOOS ?= $(shell go env GOOS)
@@ -33,7 +33,7 @@ build: ## Golang build, support cross-compile: make build GOOS=linux GOARCH=amd6
 	@version=$$(cat VERSION); \
 	echo "Building for $(GOOS)/$(GOARCH)..."; \
 	mkdir -p bin/$(GOOS)/$(GOARCH); \
-	CGO_ENABLED=0 GOOS=$(GOOS) GOARCH=$(GOARCH) go build -o bin/$(GOOS)/$(GOARCH)/collector -ldflags "$(GO_LDFLAGS)" cmd/main.go
+	CGO_ENABLED=0 GOOS=$(GOOS) GOARCH=$(GOARCH) go build -o bin/$(GOOS)/$(GOARCH)/typo -ldflags "$(GO_LDFLAGS)" cmd/main.go
 
 .PHONY: all-platform-build
 all-platform-build: ## Build for all platforms (linux/amd64, linux/arm64, darwin/amd64, darwin/arm64, windows/amd64)
@@ -44,7 +44,7 @@ all-platform-build: ## Build for all platforms (linux/amd64, linux/arm64, darwin
 	@$(MAKE) build GOOS=darwin GOARCH=arm64
 	@$(MAKE) build GOOS=windows GOARCH=amd64
 	@echo "All platform builds completed. Binaries in bin/"
-	@find bin -type f -name collector | xargs ls -lh
+	@find bin -type f -name typo | xargs ls -lh
 
 .PHONY: go-lint
 go-lint: ## run golang lint
