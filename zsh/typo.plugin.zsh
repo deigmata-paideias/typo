8c94589bd241f47b9a4b7dcfc0a1f256b365fd28
shown <yuluo08290126@gmail.com>
2026-01-24 18:34:58 +0800

    Create typo.plugin.zsh

---
+++ zsh/typo.plugin.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+# Typo zsh plugin
+
+__typo_fix() {
+
+    # dev run
+    local typo_bin="/Users/yuluo/project/indi/typo/bin/darwin/arm64/typo"
+    
+    zle -I
+
+    "$typo_bin" run
+    
+    zle -R
+    zle reset-prompt
+}
+
+zle -N __typo_fix
+bindkey '\e\e' __typo_fix
